97b98b1ce17b77f66e0acb634f270906cb06e398
Derek Toub <derektoub@gmail.com>
2014-07-16 16:11:16 -0400

    Update p1.py

--- p1.py
+++ p1.py
@@ -14,10 +14,8 @@ print sumT
 start = time.time()
 x = 3
 y = 5
-setT = {}
+setT = {y*mult for mult in range(1,200)}
 sumT = 0
-for mult in range(1,200): # 5*200 = 1000. 200 is the bound on the multiple of 5.
-    setT.add(y*mult)
 for mult in range(1,334): # 3*333 = 999.
     setT.add(x*mult)
 elapsed = time.time() - start
